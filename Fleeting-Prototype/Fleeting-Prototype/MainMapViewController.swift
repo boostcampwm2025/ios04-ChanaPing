76b36a7b718d0d97693b9cdc2eb1ea300dd16a46
MinwooJe <59834326+MinwooJe@users.noreply.github.com>
2025-12-18 04:35:36 +0900

    feat/#8: 로테이션 렌더링을 위한 AnimatedBubbleStack 구현

--- Fleeting-Prototype/Fleeting-Prototype/MainMapViewController.swift
+++ Fleeting-Prototype/Fleeting-Prototype/MainMapViewController.swift
@@ -5,6 +5,7 @@
 //  Created by MinwooJe on 12/18/25.
 //
 
+import SwiftUI
 import UIKit
 
 import NMapsMap
@@ -40,6 +41,32 @@ final class BubbleConfiguration {
         self.isAnimating = isAnimating
         self.currentText = currentText
         self.nextText = nextText
+    }
+}
+
+struct AnimatedBubbleStack: View {
+    let currentText: String
+    let nextText: String
+    let animationProgress: Double // 0.0 ~ 1.0
+
+    var body: some View {
+        ZStack {
+            // 다음 메시지 (아래에서 위로 나타남)
+            if animationProgress > 0 {
+                Text(nextText)
+                    .opacity(animationProgress)
+                    .offset(y: (1 - animationProgress) * 30)
+            }
+
+            // 현재 메시지 (위로 사라짐)
+            Text(currentText)
+                .opacity(1 - animationProgress)
+                .offset(y: -animationProgress * 30)
+        }
+        .shadow(radius: 6, y: 2)
+        .padding()
+        .background(Color.white.opacity(0.92))
+        .clipShape(RoundedRectangle(cornerRadius: 14, style: .continuous))
     }
 }
 
