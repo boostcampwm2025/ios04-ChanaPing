0ef2b63629639d36a71761107d080de18f14a857
MinwooJe <59834326+MinwooJe@users.noreply.github.com>
2025-12-18 04:34:37 +0900

    feat/#8: BubbleConfiguration 정의

--- Fleeting-Prototype/Fleeting-Prototype/MainMapViewController.swift
+++ Fleeting-Prototype/Fleeting-Prototype/MainMapViewController.swift
@@ -9,6 +9,40 @@ import UIKit
 
 import NMapsMap
 
+final class BubbleConfiguration {
+    let marker: NMFMarker
+    let messages: [String]
+    var currentIndex: Int
+    var lastRotationTime: TimeInterval
+    var animationStartTime: TimeInterval?
+    var animationProgress: Double
+    var isAnimating: Bool
+    var currentText: String
+    var nextText: String
+
+    init(
+        marker: NMFMarker,
+        messages: [String],
+        currentIndex: Int,
+        lastRotationTime: TimeInterval,
+        animationStartTime: TimeInterval? = nil,
+        animationProgress: Double,
+        isAnimating: Bool,
+        currentText: String,
+        nextText: String
+    ) {
+        self.marker = marker
+        self.messages = messages
+        self.currentIndex = currentIndex
+        self.lastRotationTime = lastRotationTime
+        self.animationStartTime = animationStartTime
+        self.animationProgress = animationProgress
+        self.isAnimating = isAnimating
+        self.currentText = currentText
+        self.nextText = nextText
+    }
+}
+
 final class MainMapViewController: UIViewController {
 
 }
