d092efd6dff7c7ae57dcb9f2969ffd5f97418a3c
MinwooJe <59834326+MinwooJe@users.noreply.github.com>
2026-01-06 21:13:23 +0900

    feat/#82: SpaceView 정의

---
+++ Meomun/Meomun/Presentation/Scene/Space/SpaceView.swift
@@ -0,0 +1,18 @@
+//
+//  SpaceView.swift
+//  Meomun
+//
+//  Created by MinwooJe on 1/6/26.
+//
+
+import SwiftUI
+
+struct SpaceView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    SpaceView()
+}
