c3845f49b8ce20d9be3bac0321309f1603cb85c0
MinwooJe <59834326+MinwooJe@users.noreply.github.com>
2026-01-07 18:39:12 +0900

    feat/#178: PlaceSearchContainerView 구현

---
+++ Meomun/Meomun/Presentation/Scene/LeaveMessage/Component/PlaceSearchContainerView.swift
@@ -0,0 +1,54 @@
+//
+//  PlaceSearchContainerView.swift
+//  Meomun
+//
+//  Created by MinwooJe on 1/7/26.
+//
+
+import SwiftUI
+
+struct PlaceSearchContainerView<Content: View>: View {
+    private let content: () -> Content
+
+    init(
+        content: @escaping () -> Content
+    ) {
+        self.content = content
+    }
+
+    var body: some View {
+        HStack(spacing: 10) {
+            Image("placeSymbol")
+                .renderingMode(.template)
+                .resizable()
+                .scaledToFit()
+                .frame(width: 20, height: 20)
+                .clipped()
+                .foregroundStyle(Color.meomunPointColor)
+
+            content()
+        }
+        .padding(.horizontal, 14)
+        .frame(height: 44)
+        .background(
+            RoundedRectangle(cornerRadius: 12, style: .continuous)
+                .fill(Color.white.opacity(0.6))
+        )
+        .overlay(
+            RoundedRectangle(cornerRadius: 12, style: .continuous)
+                .strokeBorder(Color.meomunPrimaryColor.opacity(0.15), lineWidth: 0.5)
+        )
+        .shadow(color: .black.opacity(0.05), radius: 2)
+        .shadow(color: .black.opacity(0.05), radius: 0, x: 1, y: 1)
+        .contentShape(Rectangle())
+    }
+}
+
+#Preview {
+    @FocusState var isFocused: Bool
+
+    PlaceSearchContainerView {
+        TextField("placeHolder", text: .constant("Text"))
+            .focused($isFocused)
+    }
+}
