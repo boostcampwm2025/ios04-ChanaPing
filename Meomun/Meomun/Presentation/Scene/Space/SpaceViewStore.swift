8e1799060ac6a387a9405486e02d0e356158c535
hoon <dleognsdl1@naver.com>
2026-01-12 22:48:57 +0900

    feat/#91: 공간 뷰 메세지 데이터 로드 및 더미 데이터 추가

--- Meomun/Meomun/Presentation/Scene/Space/SpaceViewStore.swift
+++ Meomun/Meomun/Presentation/Scene/Space/SpaceViewStore.swift
@@ -6,6 +6,13 @@
 //
 
 import Combine
+import Foundation
+
+// 더미 데이터 모델
+struct SpaceMessage: Identifiable, Equatable {
+    let id: UUID
+    let text: String
+}
 
 final class SpaceViewStore: Store {
 
@@ -15,11 +22,13 @@ final class SpaceViewStore: Store {
 
     enum Action {
         case setLoading(Bool)
+        case setMessages([SpaceMessage])
     }
 
     struct State {
         var isLoading: Bool = false
         var errorMessage: String = ""
+        var messages: [SpaceMessage] = []
     }
 
     @Published var state: State = .init()
@@ -29,6 +38,7 @@ final class SpaceViewStore: Store {
             switch intent {
             case .onAppear:
                 // TODO: 서버에서 메시지 데이터 fetch
+                continuation.yield(.setMessages(loadDummyMessages()))
                 continuation.finish()
             }
         }
@@ -40,9 +50,26 @@ final class SpaceViewStore: Store {
         switch action {
         case .setLoading(let isLoading):
             newState.isLoading = isLoading
+        case .setMessages(let messages):
+            newState.messages = messages
         }
 
         return newState
+    }
+
+    private func loadDummyMessages() -> [SpaceMessage] {
+        [
+            "조용함",
+            "커피 맛있다",
+            "분위기 좋다",
+            "햇살이 좋아",
+            "좌석 편하다",
+            "음악이 좋다",
+            "집중 잘 된다",
+            "향이 좋다",
+            "라떼가 최고",
+            "여유롭다"
+        ].map { SpaceMessage(id: UUID(), text: $0) }
     }
 
 }
