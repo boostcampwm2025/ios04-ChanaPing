d3442ed1921e5b647f9b7e023e89ba6fe7581e04
Hayeon Park <ha6q6v@gmail.com>
2026-01-13 04:14:28 +0900

    fix/#195: API 명세서에 맞춰 MessageRepository 수정

--- Meomun/Meomun/Domain/Repository/MessageRepository.swift
+++ Meomun/Meomun/Domain/Repository/MessageRepository.swift
@@ -7,23 +7,21 @@
 
 import Foundation
 
-// 임시 값
 struct CreateMessageRequest: Sendable {
     let text: String
+    let location: Location
     let placeID: PlaceID?
-    let location: Location?
 }
 
 protocol MessageRepository: Sendable {
-    func createMessage(_ request: CreateMessageRequest) async throws -> Message
+    func createMessage(_ request: CreateMessageRequest) async throws
     func deleteMessage(messageID: MessageID) async throws
 
-    func reportMessage(messageID: MessageID, description: String?) async throws
+    func reportMessage(messageID: MessageID) async throws -> MessageID
 
     func saveMessage(messageID: MessageID) async throws
-    func deleteSavedMessage(messageID: MessageID) async throws
     func deleteSavedMessages(messageIDs: [MessageID]) async throws
 
-    func getNearbyMessages(center: Location, radiusMeters: Double, limit: Int?) async throws -> [Message]
-    func getPlaceMessages(placeID: PlaceID, limit: Int?, before: Date?) async throws -> [Message]
+    func getNearbyMessages(location: Location, limit: Int?) async throws -> [Message]
+    func getPlaceMessages(placeID: PlaceID, limit: Int?) async throws -> [Message]
 }
