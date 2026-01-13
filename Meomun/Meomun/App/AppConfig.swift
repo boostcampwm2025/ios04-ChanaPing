cee8c7fb7ec929fffcd08c98923a5670886f37cf
moonazn <101088300+moonazn@users.noreply.github.com>
2026-01-14 02:48:12 +0900

    chore/#102: 네이버 검색, 지오코딩 api 호출 시 필요 키 등록

--- Meomun/Meomun/App/AppConfig.swift
+++ Meomun/Meomun/App/AppConfig.swift
@@ -16,4 +16,40 @@ enum AppConfig {
         }
         return key
     }
+
+    static var naverClientId: String {
+        guard let key = Bundle.main.object(
+            forInfoDictionaryKey: "NAVER_CLIENT_ID"
+        ) as? String else {
+            fatalError("NAVER_CLIENT_ID not found")
+        }
+        return key
+    }
+
+    static var naverClientSecret: String {
+        guard let key = Bundle.main.object(
+            forInfoDictionaryKey: "NAVER_CLIENT_SECRET"
+        ) as? String else {
+            fatalError("NAVER_CLIENT_SECRET not found")
+        }
+        return key
+    }
+
+    static var naverGeocodingApiKeyId: String {
+        guard let key = Bundle.main.object(
+            forInfoDictionaryKey: "X_NCP_APIGW_API_KEY_ID"
+        ) as? String else {
+            fatalError("X_NCP_APIGW_API_KEY_ID not found")
+        }
+        return key
+    }
+
+    static var naverGeocodingApiKey: String {
+        guard let key = Bundle.main.object(
+            forInfoDictionaryKey: "X_NCP_APIGW_API_KEY"
+        ) as? String else {
+            fatalError("X_NCP_APIGW_API_KEY not found")
+        }
+        return key
+    }
 }
