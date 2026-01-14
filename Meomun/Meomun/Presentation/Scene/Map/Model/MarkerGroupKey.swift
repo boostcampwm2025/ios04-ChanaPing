4d2c1154b26c1dbf4123a8185b8b6f5a50344a54
MinwooJe <59834326+MinwooJe@users.noreply.github.com>
2026-01-14 22:11:30 +0900

    feat/#198: MarkerGroupKey 정의

---
+++ Meomun/Meomun/Presentation/Scene/Map/Model/MarkerGroupKey.swift
@@ -0,0 +1,12 @@
+//
+//  MarkerGroupKey.swift
+//  Meomun
+//
+//  Created by MinwooJe on 1/14/26.
+//
+
+/// 마커 구분을 위한 복합 키 (좌표 + 마커 타입)
+struct MarkerGroupKey: Hashable {
+    let location: Location
+    let isPlace: Bool
+}
