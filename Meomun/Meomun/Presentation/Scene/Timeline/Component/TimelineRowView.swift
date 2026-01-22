b663aaa532dabe9bdfb0a3824d0d204b637b8fa7
Hayeon Park <ha6q6v@gmail.com>
2026-01-23 04:15:14 +0900

    fix/#203: 임시 좌표 수정

--- Meomun/Meomun/Presentation/Scene/Timeline/Component/TimelineRowView.swift
+++ Meomun/Meomun/Presentation/Scene/Timeline/Component/TimelineRowView.swift
@@ -43,21 +43,21 @@ struct TimelineRowView: View {
             id: MessageID(value: UUID()),
             createdAt: Date().addingTimeInterval(-7 * 60),
             content: "[Case4] NoPlace 스택 메시지 1",
-            coordinate: Coordinate.defaultCoordinate,
+            coordinate: Coordinate.seoulCity,
             placeTag: nil
         ),
         Message(
             id: MessageID(value: UUID()),
             createdAt: Date().addingTimeInterval(-30 * 60),
             content: "[Case4] NoPlace 스택 메시지 2",
-            coordinate: Coordinate.defaultCoordinate,
+            coordinate: Coordinate.seoulCity,
             placeTag: nil
         ),
         Message(
             id: MessageID(value: UUID()),
             createdAt: Date().addingTimeInterval(-15000 * 60),
             content: "[Case4] NoPlace 스택 메시지 3",
-            coordinate: Coordinate.defaultCoordinate,
+            coordinate: Coordinate.seoulCity,
             placeTag: nil
         )
     ]
