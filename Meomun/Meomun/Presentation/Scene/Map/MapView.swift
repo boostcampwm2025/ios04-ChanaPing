8ea6e912c7a7dcc0eb49e88fc28438ccce04d7ba
Hayeon Park <ha6q6v@gmail.com>
2026-01-27 06:23:13 +0900

    feat/#282: 기본 더미 메시지 제거

--- Meomun/Meomun/Presentation/Scene/Map/MapView.swift
+++ Meomun/Meomun/Presentation/Scene/Map/MapView.swift
@@ -101,7 +101,6 @@ struct MapView: View {
             ) {
                 TimelineListView(
                     store: TimelineListStore(
-                        initialMessages: store.state.selectedNoPlaceMessages,
                         fetchRecentMessagesUseCase: FetchRecentMessagesUseCaseImpl(
                             repository: MessageRepositoryImpl()
                         )
