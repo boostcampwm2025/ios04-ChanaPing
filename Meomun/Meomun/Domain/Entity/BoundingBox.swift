0736daa10087829b516a3badbe43709a41e41640
Hayeon Park <ha6q6v@gmail.com>
2026-01-27 16:48:44 +0900

    feat/#291: BoundingBox 도메인 정의

---
+++ Meomun/Meomun/Domain/Entity/BoundingBox.swift
@@ -0,0 +1,13 @@
+//
+//  BoundingBox.swift
+//  Meomun
+//
+//  Created by Hayeon Park on 1/27/26.
+//
+
+struct BoundingBox: Equatable, Sendable {
+    let minLatitude: Double
+    let maxLatitude: Double
+    let minLongitude: Double
+    let maxLongitude: Double
+}
