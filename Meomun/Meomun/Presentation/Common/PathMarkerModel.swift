03613cb21e0803b1a872ca5f277a998a4d1dc8f6
Hayeon Park <ha6q6v@gmail.com>
2026-01-29 04:44:45 +0900

    feat/#268: 경로 마커 모델 추가

---
+++ Meomun/Meomun/Presentation/Common/PathMarkerModel.swift
@@ -0,0 +1,13 @@
+//
+//  PathMarkerModel.swift
+//  Meomun
+//
+//  Created by Hayeon Park on 1/29/26.
+//
+
+import NMapsMap
+
+struct PathMarkerModel {
+    let positions: [NMGLatLng]
+    let dayLabels: [String]
+}
