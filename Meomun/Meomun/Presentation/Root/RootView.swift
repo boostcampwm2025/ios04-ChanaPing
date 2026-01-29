3e664a6f5f2ace666f44041cc46e6535d800df76
hoon <dleognsdl1@naver.com>
2026-01-30 00:57:32 +0900

    refactor/#309: RootView에서 위치 게이트 분기 제거하고 전역 권한 알럿으로 통일

--- Meomun/Meomun/Presentation/Root/RootView.swift
+++ Meomun/Meomun/Presentation/Root/RootView.swift
@@ -5,31 +5,45 @@
 //  Created by Hayeon Park on 12/19/25.
 //
 
+import CoreLocation
 import SwiftUI
 
 struct RootView: View {
     @StateObject private var locationProvider = LocationProvider()
     @Environment(\.scenePhase) private var scenePhase
-    @State private var didStartContinuous = false
-    @State private var userLocation: Coordinate?
+
+    @State private var showLocationAlert = false
+
+    private let appSettingsOpener = AppSettingsOpener()
 
     var body: some View {
-        Group {
-            if let userLocation {
-                MainTabShellView()
-            } else {
-                MainTabShellView()
-                    .overlay {
-                        LocationGateView(
-                            onReady: { coordinate in
-                                self.userLocation = coordinate
-                            },
-                            appSettingsOpener: AppSettingsOpener())
+        MainTabShellView()
+            .environmentObject(locationProvider)
+            .ignoresSafeArea(.keyboard)
+            .overlay {
+                if showLocationAlert {
+                    AlertView(type: .location) {
+                        Task { await appSettingsOpener.openAppSettings() }
                     }
+                }
             }
-        }
-        .environmentObject(locationProvider)
-        .ignoresSafeArea(.keyboard)
+            .onReceive(locationProvider.$authorizationStatus) { status in
+                switch status {
+                case .denied, .restricted:
+                    showLocationAlert = true
+                case .authorizedWhenInUse, .authorizedAlways:
+                    showLocationAlert = false
+                default:
+                    break
+                }
+            }
+            .onChange(of: scenePhase) { _, newPhase in
+                guard newPhase == .active else { return }
+                locationProvider.requestAuthorizationIfNeeded()
+
+                let status = locationProvider.authorizationStatus
+                showLocationAlert = (status == .denied || status == .restricted)
+            }
     }
 }
 
