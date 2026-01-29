8be19ec37175245d288fc88aa8d884046c314f84
MinwooJe <59834326+MinwooJe@users.noreply.github.com>
2026-01-30 04:32:03 +0900

    feat/#313: 컬러셋 추가

--- Meomun/Meomun/Presentation/Common/Extension/Color+hex.swift
+++ Meomun/Meomun/Presentation/Common/Extension/Color+hex.swift
@@ -8,15 +8,6 @@
 import SwiftUI
 
 extension Color {
-    static let meomunWriteButton = Color(hex: "#5C9397")
-    static let meomunPrimary = Color(hex: "#0E1B1A")
-    static let meomunSecondary = Color(hex: "#789290")
-    static let meomunPoint = Color(hex: "#1DC9C0")
-    static let meomunMessageBackground = Color(hex: "#FAFAFA")
-    static let meomunBackground = Color(hex: "#F4F4F4")
-}
-
-extension Color {
     init(hex: String) {
         let hex = hex.replacingOccurrences(of: "#", with: "")
         let scanner = Scanner(string: hex)
