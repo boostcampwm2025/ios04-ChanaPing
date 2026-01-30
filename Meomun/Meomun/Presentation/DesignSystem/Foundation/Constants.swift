896b0817371bd1cc750327a9a41abec7e06c49b7
MinwooJe <59834326+MinwooJe@users.noreply.github.com>
2026-01-30 23:11:09 +0900

    feat/#313: 전역 레이아웃 상수 정의

---
+++ Meomun/Meomun/Presentation/DesignSystem/Foundation/Constants.swift
@@ -0,0 +1,32 @@
+//
+//  Constants.swift
+//  Meomun
+//
+//  Created by MinwooJe on 1/30/26.
+//
+
+import SwiftUI
+
+enum MMSpacing {
+    /// Floating 컴포넌트(탭바, 캐러셀 등)의 좌우 여백
+    static let floatingHorizontalPadding: CGFloat = 30
+
+    /// Floating 컨테이너 내부 수직 여백
+    static let floatingVerticalPadding: CGFloat = 22
+
+    /// Floating 컨테이너 내부 수평 여백
+    static let floatingInnerHorizontalPadding: CGFloat = 22
+}
+
+enum MMCornerRadius {
+    static let floatingContainerTop: CGFloat = 24
+    static let floatingContainerBottom: CGFloat = 10
+}
+
+enum MMLayout {
+    /// 탭바 위에 컴포넌트를 올릴 때 사용하는 bottom offset (탭바 + 여유 공간)
+    static let aboveTabBarOffset: CGFloat = 90
+
+    /// 탭바 높이를 고려한 기본 bottom offset
+    static let tabBarBottomOffset: CGFloat = 96
+}
