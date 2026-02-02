c0fbdfd31cd65ecb2c7b0e85894ca3e603c60309
hoon <dleognsdl1@naver.com>
2026-02-03 03:41:22 +0900

    feat/#352: 네비게이션바 및 탭바 숨기기

--- Meomun/Meomun/Presentation/Scene/Setting/SettingView.swift
+++ Meomun/Meomun/Presentation/Scene/Setting/SettingView.swift
@@ -12,6 +12,8 @@ import SwiftUI
 struct SettingView: View {
     @StateObject private var store: SettingStore
     @Environment(\.scenePhase) private var scenePhase
+    @Environment(\.setTabBarHidden) private var setTabBarHidden
+    @Environment(\.setNavBarHidden) private var setNavBarHidden
     @State private var permissionStatus = LocationPermissionStatus.current
 
     private let mapView: NMFMapView = .init()
@@ -30,14 +32,26 @@ struct SettingView: View {
 
                     NavigationRow(title: "이용약관") {
                         TermsOfServiceView()
+                            .onAppear {
+                                setTabBarHidden(true)
+                                setNavBarHidden(true)
+                            }
                     }
 
                     NavigationRow(title: "개인정보처리방침") {
                         PrivacyPolicyView()
+                            .onAppear {
+                                setTabBarHidden(true)
+                                setNavBarHidden(true)
+                            }
                     }
 
                     NavigationRow(title: "오픈소스 라이선스") {
                         OpenSourceLicensesView()
+                            .onAppear {
+                                setTabBarHidden(true)
+                                setNavBarHidden(true)
+                            }
                     }
 
                     ActionRow(title: "네이버 지도 법적 공지") {
@@ -93,6 +107,8 @@ struct SettingView: View {
         }
         .onAppear {
             permissionStatus = LocationPermissionStatus.current
+            setTabBarHidden(false)
+            setNavBarHidden(false)
         }
         .onChange(of: scenePhase) { _, newPhase in
             if newPhase == .active {
