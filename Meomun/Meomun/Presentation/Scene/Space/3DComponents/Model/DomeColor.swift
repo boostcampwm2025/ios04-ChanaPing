073a1d56249ff7afeb8a3d8b963944033aaf8052
moonazn <101088300+moonazn@users.noreply.github.com>
2026-02-05 18:39:22 +0900

    refactor/#188: 돔 색상 변경

--- Meomun/Meomun/Presentation/Scene/Space/3DComponents/Model/DomeColor.swift
+++ Meomun/Meomun/Presentation/Scene/Space/3DComponents/Model/DomeColor.swift
@@ -13,16 +13,17 @@ enum DomeColor {
 
     static func colors(for dayPart: DayPart) -> GradientPair {
         switch dayPart {
+
         case .dawn:
             return (
-                top: UIColor(red: 0.18, green: 0.11, blue: 0.31, alpha: 1.0),    // 진보라
-                bottom: UIColor(red: 0.91, green: 0.63, blue: 0.75, alpha: 1.0) // 분홍
+                top: UIColor(red: 0.16, green: 0.14, blue: 0.28, alpha: 1.0),    // 잔잔한 남보라
+                bottom: UIColor(red: 0.42, green: 0.36, blue: 0.52, alpha: 1.0) // 흐린 라일락
             )
 
         case .daybreak:
             return (
-                top: UIColor(red: 0.96, green: 0.64, blue: 0.73, alpha: 1.0),    // 연분홍
-                bottom: UIColor(red: 1.00, green: 0.85, blue: 0.62, alpha: 1.0) // 살구
+                top: UIColor(red: 0.55, green: 0.42, blue: 0.50, alpha: 1.0),    // 차분한 로즈
+                bottom: UIColor(red: 0.80, green: 0.68, blue: 0.64, alpha: 1.0) // 미묘한 살구 베이지
             )
 
         case .morning:
@@ -39,14 +40,14 @@ enum DomeColor {
 
         case .evening:
             return (
-                top: UIColor(red: 0.42, green: 0.20, blue: 0.51, alpha: 1.0),    // 보라
-                bottom: UIColor(red: 0.96, green: 0.69, blue: 0.25, alpha: 1.0) // 주황
+                top: UIColor(red: 0.48, green: 0.41, blue: 0.44, alpha: 1.0),    // 따뜻한 로지 그레이 공기
+                bottom: UIColor(red: 0.90, green: 0.74, blue: 0.58, alpha: 1.0) // 밝은 노을 잔광
             )
 
         case .night:
             return (
-                top: UIColor(red: 0.05, green: 0.11, blue: 0.16, alpha: 1.0),    // 거의검정
-                bottom: UIColor(red: 0.11, green: 0.16, blue: 0.22, alpha: 1.0) // 진남
+                top: UIColor(red: 0.06, green: 0.08, blue: 0.14, alpha: 1.0),    // 딥 네이비
+                bottom: UIColor(red: 0.14, green: 0.18, blue: 0.26, alpha: 1.0) // 숨 쉬는 어둠
             )
         }
     }
